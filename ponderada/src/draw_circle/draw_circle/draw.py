8529501f7c823c255099eaacbc6cc0376633811e
AntonioArtimonte <antonio.guimaraes@sou.inteli.edu.br>
2024-05-02 12:04:09 -0300

    add: comentarios

--- ponderada/src/draw_circle/draw_circle/draw.py
+++ ponderada/src/draw_circle/draw_circle/draw.py
@@ -8,24 +8,32 @@ import tty
 import termios
 import time
 
+
 class DriverNode(Node):
+    # Classe que controla a tartaruga
     def __init__(self):
         super().__init__('driver_node')
+        # Criação dos clientes para os serviços
         self.spawn_client = self.create_client(Spawn, 'spawn')
         self.kill_client = self.create_client(Kill, 'kill')
 
+        # Variáveis de controle da caneta
         self.pen_r = 255
         self.pen_g = 210
         self.pen_b = 20
         self.pen_width = 15
         self.pen_off = False
 
+        # Spawna a tartaruga
         self.spawn_turtle()
 
+    # Método para spawnar a tartaruga
     def spawn_turtle(self):
         self.get_logger().info('Aguardando serviço de Spawn...')
         self.spawn_client.wait_for_service()
+        # Criação do request para o serviço de spawn
         spawn_request = Spawn.Request()
+        # Configuração da posição inicial da tartaruga
         spawn_request.x = 2.0
         spawn_request.y = 2.0
         spawn_request.theta = 0.0
@@ -33,19 +41,23 @@ class DriverNode(Node):
         future = self.spawn_client.call_async(spawn_request)
         rclpy.spin_until_future_complete(self, future)
         try:
+            # Verifica se o serviço foi executado com sucesso
             response = future.result()
             self.turtle_name = response.name
             self.get_logger().info(f'Tartaruga de nome: {self.turtle_name} spawnada')
             self.setup_turtle_controls()
+            # Se o serviço falhar, printa o erro
         except Exception as e:
             self.get_logger().error('Falha na hora de spawnar: %r' % (e,))
 
+    # Método para configurar os controles da tartaruga
     def setup_turtle_controls(self):
         self.publisher_ = self.create_publisher(Twist, f'/{self.turtle_name}/cmd_vel', 10)
         self.pen_client = self.create_client(SetPen, f'/{self.turtle_name}/set_pen')
         self.timer = self.create_timer(1, self.timer_callback)
         self.set_initial_pen_settings()
 
+    # Método para controlar a tartaruga
     def timer_callback(self):
         msg = Twist()
         linear_vel = 1.3
@@ -55,6 +67,7 @@ class DriverNode(Node):
         msg.angular.z = linear_vel / radius
         self.publisher_.publish(msg)
 
+    # Método para setar as configurações iniciais da caneta
     def set_initial_pen_settings(self):
         self.get_logger().info('Aguardando serviço de SetPen...')
         self.pen_client.wait_for_service()
@@ -73,6 +86,7 @@ class DriverNode(Node):
         except Exception as e:
             self.get_logger().error('Setagem da caneta falhada: %r' % (e,))
 
+    # Método para matar a tartaruga
     def kill_turtle(self):
         self.get_logger().info('Esperando serviço de Kill...')
         self.kill_client.wait_for_service()
@@ -86,20 +100,26 @@ class DriverNode(Node):
         except Exception as e:
             self.get_logger().error(f'Falha na morte: {e}')
 
+# Função principal
 def main(args=None):
+    # Inicialização do ROS
     rclpy.init(args=args)
+    # Criação do nó
     node = DriverNode()
     original_settings = termios.tcgetattr(sys.stdin)
     try:
         tty.setcbreak(sys.stdin.fileno())
         while rclpy.ok():
+            # Verifica se a tecla Q foi pressionada
             if select.select([sys.stdin], [], [], 0)[0]:
+                # Se sim, mata a tartaruga
                 if sys.stdin.read(1) == 'q':
                     node.get_logger().info('Tecla Q pressionada, matando a tartaruga...')
                     node.kill_turtle()
                     time.sleep(15)
                     break
             rclpy.spin_once(node, timeout_sec=0.1)
+            # Se a tartaruga for morta, fecha o terminal
     finally:
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, original_settings)
         rclpy.shutdown()
